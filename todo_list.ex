a6481af38e4c5c0e145718e5133ded0a8a4cb924
Bobby Lei <bobl53@gmail.com>
2016-11-05 16:57:34 +0800

    prebuidling

--- todo_list.ex
+++ todo_list.ex
@@ -1,7 +1,13 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: HashDict.new
 
-  def new, do: %TodoList{}
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
 
   def add_entry(
     %TodoList{entries: entries, auto_id: auto_id} = todo_list,
