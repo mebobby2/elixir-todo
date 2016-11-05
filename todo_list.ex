117b9ffed9a6c596f0f993fed6804c56a2020bbd
Bobby Lei <bobl53@gmail.com>
2016-11-05 17:26:10 +0800

    list comprehensions

--- todo_list.ex
+++ todo_list.ex
@@ -57,3 +57,23 @@ defmodule TodoList do
     %TodoList{todo_list | entries: HashDict.delete(entries, entry_id)}
   end
 end
+
+# Example to use in list comprehensions
+# entries = [
+#           %{date: {2013, 12, 19}, title: "Dentist"},
+#           %{date: {2013, 12, 20}, title: "Shopping"},
+#           %{date: {2013, 12, 19}, title: "Movies"}
+# ]
+# for entry <- entries, into: TodoList.new, do: entry
+
+defimpl Collectable, for: TodoList do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+end
