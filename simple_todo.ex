67350288468cf19b0f0dbdeea49b82fc8a92f44f
Bobby Lei <bobl53@gmail.com>
2016-11-05 15:08:08 +0800

    using a map instead of a tuple so when we change the format we store the entries, it does not break the function signatures

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,8 +1,8 @@
 defmodule TodoList do
   def new, do: MultiDict.new
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, title)
   end
 
   def entries(todo_list, date) do
