0f1a293c7c36f03c7b0c3e8ebe4026b08004a18c
Bobby Lei <bobl53@gmail.com>
2016-11-05 15:05:20 +0800

    moved the functionality of storing multiple entries against a key into a separate abstraction

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,16 +1,11 @@
 defmodule TodoList do
-  def new, do: HashDict.new
+  def new, do: MultiDict.new
 
   def add_entry(todo_list, date, title) do
-    HashDict.update(
-      todo_list,
-      date,
-      [title],
-      fn(titles) -> [title | titles] end
-    )
+    MultiDict.add(todo_list, date, title)
   end
 
   def entries(todo_list, date) do
-    HashDict.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
   end
 end
