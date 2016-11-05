b8d43cc99757116f67619c0e63f86148e12dda4a
Bobby Lei <bobl53@gmail.com>
2016-11-05 15:22:22 +0800

    fix typo

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # elixir-todo
 
+## To Run
+
+* iex
+* c(["simple_todo.ex", "todo_multi_dict.ex"])
+* l = TodoList.new |> TodoList.add_entry(%{date: {2013,12,19}, title: "Dentist"})
+* TodoList.entries(l, {2013,12,19})
+
 Upto page 104
--- simple_todo.ex
+++ simple_todo.ex
@@ -2,7 +2,7 @@ defmodule TodoList do
   def new, do: MultiDict.new
 
   def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, title)
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
   def entries(todo_list, date) do
