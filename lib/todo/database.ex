2bb4bd57413a7c1d7836b4047cc360af715fd74a
Bobby Lei <bobl53@gmail.com>
2016-11-17 16:00:14 +0800

    linking all processes so no dangling processes are left behind

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,8 +1,10 @@
 defmodule Todo.Database do
   use GenServer
 
-  def start(db_folder) do
-    GenServer.start(__MODULE__, db_folder, name: :database_server)
+  def start_link(db_folder) do
+    IO.puts "Starting database server."
+
+    GenServer.start_link(__MODULE__, db_folder, name: :database_server)
   end
 
   def store(key, data) do
@@ -27,7 +29,7 @@ defmodule Todo.Database do
 
   defp start_workers(db_folder) do
     for index <- 1..3, into: HashDict.new do
-      {:ok, pid} = Todo.DatabaseWorker.start(db_folder)
+      {:ok, pid} = Todo.DatabaseWorker.start_link(db_folder)
       {index - 1, pid}
     end
   end
