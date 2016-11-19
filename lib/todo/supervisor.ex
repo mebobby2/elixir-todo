78929639bc55824aed6dfb40b7d477c830a03180
Bobby Lei <bobl53@gmail.com>
2016-11-19 21:50:01 +0800

    add in a system supervisor to ensure that when the process registry crashes, it restarts the rest of the system

--- lib/todo/supervisor.ex
+++ lib/todo/supervisor.ex
@@ -8,10 +8,11 @@ defmodule Todo.Supervisor do
   def init(_) do
     processes = [
       worker(Todo.ProcessRegistry, []),
-      supervisor(Todo.Database, ["./persist/"]),
-      supervisor(Todo.ServerSupervisor, []),
-      worker(Todo.Cache, [])
+      supervisor(Todo.SystemSupervisor, [])
     ]
-    supervise(processes, strategy: :one_for_one)
+
+    # We use a :rest_for_one strategy, thus ensuring that a crash of the
+    # process registry takes down the entire system
+    supervise(processes, strategy: :rest_for_one)
   end
 end
