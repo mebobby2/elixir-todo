cda92f8af167449611890ca170dd1927495d5747
Bobby Lei <bobl53@gmail.com>
2016-11-26 10:18:41 +0800

    adding cowboy and plug

--- mix.exs
+++ mix.exs
@@ -34,7 +34,10 @@ defmodule Todo.Mixfile do
   defp deps do
     [
       {:gproc, "0.3.1"},
-      {:meck, "0.8.2", only: :test}
+      {:cowboy, "1.0.0"},
+      {:plug, "0.10.0"},
+      {:meck, "0.8.2", only: :test},
+      {:httpoison, "0.4.3", only: :test}
     ]
   end
 end
