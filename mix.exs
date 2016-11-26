1928776e9e54700ad73824f6ab2c749c186ab22d
Bobby Lei <bobl53@gmail.com>
2016-11-26 09:39:57 +0800

    turn the todo app into an OTP application

--- mix.exs
+++ mix.exs
@@ -2,19 +2,24 @@ defmodule Todo.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :todo,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :todo,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger],
+      mod: {Todo.Application, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +32,8 @@ defmodule Todo.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:meck, "0.8.2", only: :test}
+    ]
   end
 end
